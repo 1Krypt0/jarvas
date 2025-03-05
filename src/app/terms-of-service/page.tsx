e4c83bf5bcac04e19d4912d0ac34dfc1df38b370
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2025-03-05 14:15:54 +0000

    Update privacy policy

--- src/app/terms-of-service/page.tsx
+++ src/app/terms-of-service/page.tsx
@@ -3,7 +3,7 @@ import ToS from "@/markdown/tos.mdx";
 export default function TermsOfService() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <div className="flex flex-col w-1/2 pt-24 gap-3">
+      <div className="flex flex-col w-1/2 py-24 gap-3">
         <ToS />
       </div>
     </main>
