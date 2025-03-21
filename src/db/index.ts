8dd3098e8874f9c00194d8ac028b11f45f82b05f
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2025-03-21 15:40:15 +0000

    Migrate db over to neon

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import { env } from "@/env";
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
 
 export const db = drizzle(env.DATABASE_URL, {
   casing: "snake_case",
